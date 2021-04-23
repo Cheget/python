4cb50b8ada12d95b74f729021317f146251b485c
Cheget <georgeborz@gmail.com>
2021-04-23 16:52:03 +0300

    Update 3.5.py
    exercise 3.5 done

--- 3rdLesson/3.5.py
+++ 3rdLesson/3.5.py
@@ -1,11 +1,18 @@
-def sum_numbers(my_list):
+def sum_numbers(my_list, z):
     test1 = [int(x) for x in my_list.split() if x.isdigit()]
-    return sum(test1)
+    print(z + sum(test1))
+    return z + sum(test1)
+
+z = 0
 while True:
     my_list = input('введите числа')
-    if (x.isdigit() in my_list) == False:
-       z = z + sum_numbers(my_list) 
-       break 
-    else:
-        z = z + sum_numbers(my_list) 
-print()
+    my_list_to_list = list(my_list)
+    exit = False
+    for i in my_list_to_list:
+        if i == 'e':
+            exit = True
+    if exit == True:
+        z = sum_numbers(my_list, z) 
+        break
+    z = sum_numbers(my_list, z) 
+    
